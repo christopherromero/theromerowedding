d9db4f7beb92016f434f15405785fbfbc4bcc3c9
Christopher Romero <christopherromero11@gmail.com>
2019-01-25 09:27:45 -0800

    Initial commit

---
+++ theromerowedding/Pages/Directions.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace theromerowedding.Pages
+{
+    public class DirectionsModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Directions to The Wedding.";
+        }
+    }
+}
---
+++ theromerowedding/Pages/TheWedding.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace theromerowedding.Pages
+{
+    public class TheWeddingModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Explore the Venue";
+        }
+    }
+}
